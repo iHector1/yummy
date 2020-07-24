76f6f47d7739e36cfa916a65bdeb5082405ca608
iHector1 <62352013+iHector1@users.noreply.github.com>
2020-07-24 12:46:06 -0500

    diseño de navbar listo

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component  } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent  {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );  
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
