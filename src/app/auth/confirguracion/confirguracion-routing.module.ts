b9db6507699681a81d52c494b71d9e20c1580e4c
iHector1 <62352013+iHector1@users.noreply.github.com>
2020-09-10 23:25:50 -0500

    vista de llenado de datos hecho

--- src/app/auth/confirguracion/confirguracion-routing.module.ts
+++ src/app/auth/confirguracion/confirguracion-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { ConfirguracionComponent } from './confirguracion.component';
 
 const routes: Routes = [{ path: '', component: ConfirguracionComponent }];
--- src/app/auth/confirguracion/confirguracion.module.ts
+++ src/app/auth/confirguracion/confirguracion.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ConfirguracionRoutingModule } from './confirguracion-routing.module';
 import { ConfirguracionComponent } from './confirguracion.component';
-
+import {AngularMaterialModule } from '../../Angular_Material/angular_materia.module';
 
 @NgModule({
   declarations: [ConfirguracionComponent],
   imports: [
     CommonModule,
-    ConfirguracionRoutingModule
+    ConfirguracionRoutingModule,
+    AngularMaterialModule
   ]
 })
 export class ConfirguracionModule { }
