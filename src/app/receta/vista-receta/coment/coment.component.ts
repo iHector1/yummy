6f412a5f6e41a67d70e13f1c715a1be8f92fc1ee
iHector1 <62352013+iHector1@users.noreply.github.com>
2020-10-10 12:27:06 -0500

    crear comentario hecho

---
+++ src/app/receta/vista-receta/coment/coment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coment',
+  templateUrl: './coment.component.html',
+  styleUrls: ['./coment.component.css']
+})
+export class ComentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/receta/vista-receta/vista-receta.module.ts
+++ src/app/receta/vista-receta/vista-receta.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { VistaRecetaRoutingModule } from './vista-receta-routing.module';
 import { VistaRecetaComponent } from './vista-receta.component';
 import { AngularMaterialModule } from '../../Angular_Material/angular_materia.module';
+import { ComentComponent } from './coment/coment.component';
 
 @NgModule({
-  declarations: [VistaRecetaComponent],
+  declarations: [VistaRecetaComponent, ComentComponent],
   imports: [
     CommonModule,
     VistaRecetaRoutingModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+
   ]
 })
 export class VistaRecetaModule { }
