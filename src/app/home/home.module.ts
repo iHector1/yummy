381c143cffe3c84e29842772269c46ed6c39a39a
iHector1 <62352013+iHector1@users.noreply.github.com>
2020-10-12 14:30:30 -0500

    index terminado

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,13 +6,15 @@ import { HomeComponent } from './home.component';
 import { PopularComponent } from './popular/popular.component';
 import { NewestComponent } from './newest/newest.component';
 import { MonthComponent } from './month/month.component';
+import { AngularMaterialModule } from '../Angular_Material/angular_materia.module';
 
 
 @NgModule({
   declarations: [HomeComponent, PopularComponent, NewestComponent, MonthComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    AngularMaterialModule
   ]
 })
 export class HomeModule { }
