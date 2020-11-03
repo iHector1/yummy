3cc62fa45838e470ca3f46861bdc7cca6b9de11d
iHector1 <62352013+iHector1@users.noreply.github.com>
2020-11-03 11:33:49 -0600

    ingredientes y utencilios al 100

---
+++ src/app/ingredients/service/ingredients-service.service.ts
@@ -0,0 +1,96 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable, of } from 'rxjs';
+import { ingredient} from '../../shared/models/ingredients.interface';
+@Injectable({
+  providedIn: 'root'
+})
+export class IngredientsServiceService {
+  private uidIngredient: any;
+  private flag: boolean = false;
+  private flag2: boolean = true;
+  private  uidkindfood;
+  public ingredient$: Observable<ingredient>;//Variable para guardar el ingrediente
+  constructor(private afs: AngularFirestore) { }
+  uid: any;
+  public ingredientCollection(ingredient: ingredient, uiserr: any, food: any): void {
+    
+      this.afs.collection('cookWare', ref => ref.where('nameCookWare', '==', ingredient.nameIngredient)).valueChanges().subscribe(users => {
+        if (users[0]) {
+         if (this.flag == false) {
+            this.uidIngredient = users[0];
+           this.uid = this.uidIngredient["uid"];
+           //verifica que el usuario no haya hecho solicitud otra vez 
+           if (!this.uidIngredient["request"].some(x => x === uiserr)) {
+             this.uidIngredient["request"].push(uiserr);
+             console.log(this.uid, "  ", this.uidIngredient["request"]);
+             this.ingredientDataUpdate(this.uidIngredient["request"]); 
+             window.alert("Haz hecho una  solicitud a este ingrediente ");
+             this.flag2 = false;
+           } else {
+             window.alert("Ya has hecho solicitud a este ingrediente,intenta con otro ");
+           }
+          }
+        } else {
+          //console.log("no xisto");
+          //si no existe agrega el ingrediente
+          this.ingredientDataAdd(ingredient, food);
+          this.ingredientDataAddtable(ingredient);
+          window.alert("Haz hecho una  solicitud a este ingrediente ");
+          this.flag2 = false;
+        }
+      });
+  }
+  ingredientDataUpdate(request: any) {
+    this.flag = true;
+    console.log(this.uid, "  ", request);
+   this.afs.collection("cookWare")
+    .doc(this.uid)
+      .set({ request: request }, { merge: true });
+  }
+
+
+  public ingredientDataAdd(ingredient: ingredient, food: any) {
+    console.log(food);
+    let foood = food;
+     console.log(food);
+    if (foood == "1") {
+      this.uidkindfood = "oils";
+    } else if (foood=="2") {
+      this.uidkindfood = "meats";
+    } else if (foood=="3") {
+      this.uidkindfood = "cereals";
+    } else if (foood=="4") {
+      this.uidkindfood = "vegetables";
+    }
+    else if (foood=="5") {
+      this.uidkindfood = "fruits";
+    }
+    this.flag = true;
+    console.log(ingredient);
+    const userRef: AngularFirestoreDocument<ingredient> = this.afs.doc(
+      `${this.uidkindfood}/${ingredient.uid}`
+    );
+    const data: any = {
+      uid: ingredient.uid,
+      uidIngredient:ingredient.uid
+    };
+    return userRef.set(data, { merge: true });
+
+  }
+  ingredientDataAddtable(ingredient:ingredient) {
+    console.log(ingredient);
+    const userRef: AngularFirestoreDocument<ingredient> = this.afs.doc(
+      `ingredients/${ingredient.uid}`
+    );
+    const data: ingredient = {
+      uid: ingredient.uid,
+      uidUnit: ingredient.uidUnit,
+      nameIngredient: ingredient.nameIngredient,
+      request : ingredient.request
+    };
+    return userRef.set(data, { merge: true });
+  }
+}
+  
+
---
+++ src/app/shared/models/ingredients.interface.ts
@@ -0,0 +1,8 @@
+export interface ingredient{
+    uid?: string;
+    uidUnit?: string;
+    kindFoodName?: string;
+    nameIngredient?:string;
+    request?: string[];
+  }
+  
