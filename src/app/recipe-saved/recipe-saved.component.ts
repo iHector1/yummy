3ff8ac52e1e8c0faf526c1ac185ad85fdd540809
LKaren <64727353+LKaren@users.noreply.github.com>
2020-11-18 18:48:19 -0600

    Recipe saved, recipe, search

--- src/app/recipe-saved/recipe-saved.component.ts
+++ src/app/recipe-saved/recipe-saved.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'firebase';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth/services/auth.service';
+import { infoRecipe } from '../shared/models/infoRecipe.interface';
+import { RecipeSavedService } from './service/recipe-saved.service';
 
 @Component({
   selector: 'app-recipe-saved',
@@ -7,7 +16,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeSavedComponent implements OnInit {
 
-  constructor() { }
+  category: Observable<any[]>;// Variable para recibir la categoría de la receta
+
+  //variables del formulario
+  recipeSavedForm = new FormGroup({
+    recipeSavedCategory: new FormControl('', [
+    ]),
+  });
+
+  constructor(firestore:AngularFirestore, private storage: AngularFireStorage, private recipeSavedService:RecipeSavedService,private authService:AuthService,private router:Router)
+  {
+    this.category = firestore.collection('category').valueChanges();
+   }
+
+  @ViewChild('idUser') inputUserid: ElementRef;
+  public user$: Observable<User> = this.authService.afAuth.user;
 
   ngOnInit(): void {
   }
--- src/app/search-recipes/search/search.component.ts
+++ src/app/search-recipes/search/search.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Router } from '@angular/router';
+import { User } from 'firebase';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +13,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(firestore:AngularFirestore, private storage: AngularFireStorage, private authService:AuthService,private router:Router)
+  {
+  }
+
+  @ViewChild('idUser') inputUserid: ElementRef;
+  public user$: Observable<User> = this.authService.afAuth.user;
 
   ngOnInit(): void {
   }
+
 
 }
