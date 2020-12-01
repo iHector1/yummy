0f608027308b7e18e4ac3523a6644a986d2869d8
iHector1 <62352013+iHector1@users.noreply.github.com>
2020-12-01 12:00:59 -0600

    userprofile hecho

--- src/app/info-user/recipes/recipes.component.ts
+++ src/app/info-user/recipes/recipes.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { infoRecipe } from 'src/app/shared/models/infoRecipe.interface';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-
-  constructor() { }
-
+  
+  recipe: Observable<infoRecipe[]>;
+  displayName: any;
+  constructor(private firestore: AngularFirestore,private router: Router, private auth: AuthService) { }
+ 
   ngOnInit(): void {
+    this.recipe = this.firestore.collection("infoRecipe", ref => ref.where("uidUser", "==", this.router.url.slice(9))).valueChanges();
+
+    this.auth.getUser(this.router.url.slice(9)).subscribe(user => {
+      if (user[0]) {
+        const uiUser: any = user[0];
+        this.displayName = uiUser.displayName; 
+      }
+    });
+
   }
 
 }
--- src/app/info-user/user/user.component.ts
+++ src/app/info-user/user/user.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { FollowService } from 'src/app/auth/services/follow.service';
 import { ChatService } from 'src/app/chat/service/chat.service';
+import { User } from 'src/app/shared/models/user.inteface';
 
 @Component({
   selector: 'app-user',
@@ -18,11 +20,22 @@ export class UserComponent implements OnInit {
   totalFollowers: number;
   totalFollowing: number;
   totalRecipes: number;
+  isFollowing: any;
+  isUser: any;
+  show: boolean;
+  isUserName: any;
+  premium: boolean;
+  useruid: any;
+  colorButton: string;
 
+  public user$: Observable<User> = this.auth.afAuth.user;
   constructor(private firestore: AngularFirestore, private router: Router, private auth: AuthService, private follow: FollowService, private chat: ChatService) { }
 
   ngOnInit(): void {
-    this.auth.getUser(this.router.url.slice(9)).subscribe(user => {
+    this.useruid = this.router.url.slice(9);
+    this.isFollowing = false;
+    this.premium = false;
+    this.auth.getUser(this.useruid).subscribe(user => {
       if (user[0]) {
         const userVar: any = user[0];
         this.principalPhoto = userVar.photoUrl;
@@ -40,7 +53,7 @@ export class UserComponent implements OnInit {
             this.levelName = levelVar.levelName;
           }
         });
-
+        this.user(this.useruid);
         this.recipes();
         this.followersandFollowings();
       }
@@ -62,5 +75,60 @@ export class UserComponent implements OnInit {
       subscribe(recipes => {
         this.totalRecipes = recipes.length;
     })
+  }
+  followUser() {
+    if (this.isFollowing) {
+      this.isFollowing = false;
+      this.follow.unfollow(this.useruid);
+    } else {
+      this.isFollowing = true;
+      this.follow.follow(this.useruid);
+      this.chat.createChat(this.useruid,this.isUser,this.displayName,this.isUserName)
+    }
+  }
+  user(uid) {
+    this.auth.getUser(uid).subscribe(user => {
+      if (user[0]) {
+        const uiUser: any = user[0];
+        this.displayName = uiUser.displayName;
+        this.user$.subscribe(user => {
+          this.isUser = user.uid;
+          if (this.isUser == uiUser.uid || this.isUser == null || this.isUser == undefined || this.isUser == "") {
+            this.show = false;
+           
+          }
+          else {
+            this.show = true;
+            this.follow.isFollowing(uid, this.isUser).subscribe(
+              followinguser => {
+                if (followinguser[0]) {
+                  this.isFollowing = true;
+                  console.log("sie entro ");
+                }
+              });
+            
+          }
+          this.auth.getUser(this.isUser).subscribe(user2 => {
+            if (user2[0]) {
+              const uiUser2: any = user2[0];
+              this.isUserName = uiUser2.displayName;
+            }
+          })
+          console.log("primero: ", uiUser.uid, "segundo: ", this.isUser);
+          console.log(this.show);
+        });
+        
+      }
+      
+    });
+  }
+  checkFollowing() {
+    if (this.isFollowing) {
+      this.colorButton = "primary";
+      return 'Siguiendo';
+    } else {
+      this.colorButton = "accent";
+      return 'Seguir';
+    }
   }
 }
