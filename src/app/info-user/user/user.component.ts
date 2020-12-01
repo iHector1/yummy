1381e283b136b42019afe11715e417e5fb09153d
iHector1 <62352013+iHector1@users.noreply.github.com>
2020-12-01 08:47:26 -0600

    primeros edits de de user profile

--- src/app/info-user/user/user.component.ts
+++ src/app/info-user/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { FollowService } from 'src/app/auth/services/follow.service';
+import { ChatService } from 'src/app/chat/service/chat.service';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  principalPhoto: any;
+  displayName: any;
+  stateName: any;
+  levelName: any;
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore, private router: Router, private auth: AuthService, private follow: FollowService, private chat: ChatService) { }
 
   ngOnInit(): void {
+    this.auth.getUser(this.router.url.slice(9)).subscribe(user => {
+      if (user[0]) {
+        const userVar: any = user[0];
+        this.principalPhoto = userVar.photoUrl;
+        this.displayName = userVar.displayName;
+        this.firestore.collection("state", ref => ref.where("uid", "==", userVar.uidState)).valueChanges().subscribe(state => {
+          if (state[0]) {
+            const stateVar: any = state[0]
+            this.stateName = stateVar.stateName; 
+          }
+        });
+
+        this.firestore.collection("level", ref => ref.where("uid", "==", userVar.uidLevel)).valueChanges().subscribe(level => {
+          if (level[0]) {
+            const levelVar:any = level[0];
+            this.levelName = levelVar.levelName;
+          }
+        })
+      }
+    })
   }
 
 }
