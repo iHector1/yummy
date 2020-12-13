221bc37add514baf954591ec25ad4c66b0d40537
iHector1 <62352013+iHector1@users.noreply.github.com>
2020-12-12 23:16:53 -0600

    destacar una respuesta listo

--- src/app/receta/vista-receta/vista-comentarios/vista-respuestas/vista-respuestas.component.ts
+++ src/app/receta/vista-receta/vista-comentarios/vista-respuestas/vista-respuestas.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
 import { HelpService } from 'src/app/receta/service/help.service';
+import { User } from 'src/app/shared/models/user.inteface';
 
 @Component({
   selector: 'app-vista-respuestas',
@@ -12,18 +15,51 @@ export class VistaRespuestasComponent implements OnInit {
   @Input() helpComment: any;
   answer: any;
   answerHighlight: any;
-  constructor(private afs: AngularFirestore,private helpService:HelpService) {
-    
-   }
+  uidUser: any;
+  show: boolean;
+  constructor(private afs: AngularFirestore, private helpService: HelpService,private authService:AuthService) {
+  }
+  public user$: Observable<User> = this.authService.afAuth.user;
   limit: number = 1;
   ngOnInit(): void {
+    //respuestas no destacadas
     this.answer = this.afs.collection('answer', ref => ref.where('uidHelp', '==', this.helpComment.uid).where('request', "==", 0).orderBy('time', 'asc').limit(this.limit)).valueChanges();
+    //algoritmo para verificar el dueño de la pregunta
+    this.afs.collection('answer', ref => ref.where('uidHelp', '==', this.helpComment.uid).where('request', "==", 0).orderBy('time', 'asc').limit(this.limit)).valueChanges().subscribe(comment => {
+      
+      this.user$.subscribe(user => { 
+        this.authService.getUser(user.uid).subscribe(userInfo => { 
+          if (userInfo[0]) {
+            const info: any = userInfo[0];
+            this.uidUser = info.uid;
+          }
+          this.afs.collection('help', ref => ref.where('uid', '==', this.helpComment.uid).where('uidUser', '==', this.uidUser)).valueChanges().subscribe(help => {
+            if (help[0]) {
+              const helpp:any = help[0];
+              if (this.uidUser == helpp.uidUser || this.uidUser == undefined || this.uidUser == "") {
+                this.show = true;
+              }
+              else {
+                this.show = false;
+              }
+            }
+          })
+          
+        })
+      })
+
+    });
+
+
+//preguntas destacadas
     this.answerHighlight=this.afs.collection('answer', ref => ref.where('uidHelp', '==', this.helpComment.uid).where('request', "==", 1).orderBy('time', 'asc')).valueChanges();
   }
+  //mostrar mas respuestas
   more() {
     this.limit = this.limit + 1;
     this.answer = this.afs.collection('answer',ref=>ref.where('uidHelp','==',this.helpComment.uid).where('request',"==",0).orderBy('time','asc').limit(this.limit)).valueChanges();
   }
+  //destacar una pregunta
   Highlight(uid: any) {
     this.helpService.highlightAnswer(uid);
   }
