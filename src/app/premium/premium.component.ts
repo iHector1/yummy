c7d31b0b1caf5a83cbd8b4996d826de8b975c29b
iHector1 <62352013+iHector1@users.noreply.github.com>
2020-12-20 19:32:02 -0600

    mejora de la vista

--- src/app/premium/premium.component.ts
+++ src/app/premium/premium.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-premium',
@@ -6,8 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./premium.component.css']
 })
 export class PremiumComponent implements OnInit {
+  message: any;
+  cost: any;
+  paypalAccount: any;
+  urlVideo: any;
+  displayName: any;
 
-  constructor() { }
+  constructor(private router: Router, private afs:AngularFirestore) {
+    console.log(this.router.url.slice(13));
+    this.afs.collection('premiunAd', ref => ref.where("uid", '==', this.router.url.slice(13))).valueChanges().subscribe(ad => {
+      if (ad[0]) {
+        const adVar:any = ad[0];
+        this.message = adVar.message;
+        this.cost = adVar.cost;
+        this.paypalAccount = adVar.paypalAccount;
+        this.urlVideo = adVar.urlVideo;
+        console.log(this.urlVideo);
+        this.afs.collection('infoUser', ref => ref.where("uid", "==", adVar.uid)).valueChanges().subscribe(user => {
+          if (user[0]) {
+            const infoUser: any = user[0];
+            this.displayName = infoUser.displayName;       
+         }
+       })
+     } 
+    })
+   }
 
   ngOnInit(): void {
   }
--- src/app/receta/vista-receta/vista-comentarios/vista-comentarios.component.ts
+++ src/app/receta/vista-receta/vista-comentarios/vista-comentarios.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,10 +13,10 @@ export class VistaComentariosComponent implements OnInit {
   med: Observable<any>;
   mal: Observable<any>;
   
-  constructor(private afs: AngularFirestore) {
-    this.exce = this.afs.collection("comment", ref => ref.where("stars", ">", "3")).valueChanges();
-    this.med = this.afs.collection("comment", ref => ref.where("stars", "==","3")).valueChanges();
-    this.mal = this.afs.collection("comment", ref => ref.where("stars", "<","3")).valueChanges();
+  constructor(private afs: AngularFirestore,private router:Router) {
+    this.exce = this.afs.collection("comment", ref => ref.where("stars", ">", "3").where("uidRecipe",'==',this.router.url.slice(8))).valueChanges();
+    this.med = this.afs.collection("comment", ref => ref.where("stars", "==","3").where("uidRecipe",'==',this.router.url.slice(8))).valueChanges();
+    this.mal = this.afs.collection("comment", ref => ref.where("stars", "<","3").where("uidRecipe",'==',this.router.url.slice(8))).valueChanges();
    }
 
   ngOnInit(): void {
