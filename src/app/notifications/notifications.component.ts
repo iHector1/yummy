081afcb8870f864f619644ea38aafdf7a19c5ce2
iHector1 <62352013+iHector1@users.noreply.github.com>
2020-12-23 22:39:52 -0600

    vistas de notificaciones al 100

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth/services/auth.service';
 
 @Component({
   selector: 'app-notifications',
@@ -7,7 +11,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsComponent implements OnInit {
 
-  constructor() { }
+  noti: any;
+  constructor(private afs: AngularFirestore,private router:Router) {
+    this.noti =this.afs.collection('mail', ref => ref.where('uidUser', '==', this.router.url.slice(16)).orderBy('delivery.startTime','desc')).valueChanges();
+    this.noti.subscribe(notifi => {
+      console.log(notifi);
+    })
+   }
 
   ngOnInit(): void {
   }
